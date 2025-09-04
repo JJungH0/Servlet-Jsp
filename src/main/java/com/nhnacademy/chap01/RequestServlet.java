c2628f4146953ccf1dfe6050db962a0f09fd4628
JJung0 <junghwan__@naver.com>
2025-09-04 17:22:38 +0900

    [FEAT]: Http Servlet Request개발 실습01-HttpRequestServlet(주석추가)

--- src/main/java/com/nhnacademy/chap01/RequestServlet.java
+++ src/main/java/com/nhnacademy/chap01/RequestServlet.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.chap01;
 
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,7 +19,16 @@ public class RequestServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         resp.setContentType("text/plain; charset=UTF-8");
-
+        /**
+         * resp.getWriter() :
+         * -> HTTP 응답 바디에 "문자 텍스트"를 쓰기 위한 Writer를 반환
+         * -> 컨테이너가 응답의 인코딩(Content-Type의 charset 또는
+         *      setCharacterEncoding)과 버퍼링을 적용한 Writer를 제공
+         * PrintWriter.println(..) :
+         * -> 반환받은 Writer로 HTML 문자열을 작성
+         * -> println은 줄바꿈까지 포함해 편리하게 출력
+         * -> 최종적으로 해당 데이터가 HTTP 응답 바디로 전송
+         */
         try (PrintWriter pw = resp.getWriter()) {
             pw.println("local = " + req.getLocale());
             pw.println("parameter name = " + req.getParameter("userId"));
@@ -28,6 +39,7 @@ public class RequestServlet extends HttpServlet {
             pw.println("request uri = " + req.getRequestURI());
             pw.println("request url = " + req.getRequestURL());
             pw.println("User-Agent header = " + req.getHeader("User-Agent"));
+
         } catch (IOException e) {
             log.error("/req : {}", e.getMessage(), e);
         }
